a06d3f8d1f656d7a40dc136041a44ea7a0ace51e
hjchu <3263518006@qq.com>
2024-04-26 11:44:50 +0800

    second commit

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -5,10 +5,11 @@ package com.example.git;
  * Package: com.example.git
  * Description:
  *
- * @Create 10:57
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+
     }
 }
