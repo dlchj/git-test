5beb2280073704e1508c52a1f97bd707144d0f27
hjchu <3263518006@qq.com>
2024-04-26 11:39:43 +0800

    first commit

---
+++ src/main/java/com/example/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.example.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.example.git
+ * Description:
+ *
+ * @Create 10:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
